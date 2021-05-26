6b65ebdfbc3288626964a87be521178781a717a3
Vadzim <onreg01@gmail.com>
2021-05-26 22:05:54 +0300

    clean up gradle

---
+++ buildSrc/src/main/java/io/onreg/sdk/Configuration.kt
@@ -0,0 +1,9 @@
+package io.onreg.sdk
+
+object Configuration {
+    const val compileSdkVersion = 30
+    const val targetSdkVersion = 30
+    const val minSdkVersion = 16
+    const val versionName = "0.0.1"
+    const val artifactGroup = "io.github.onreg"
+}
