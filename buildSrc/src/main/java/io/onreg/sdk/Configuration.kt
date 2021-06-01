b748b45c58f812713a284369e882620699ee4a6d
Vadzim <onreg01@gmail.com>
2021-06-01 21:24:59 +0300

    bump to: versionName = "0.0.2"

--- buildSrc/src/main/java/io/onreg/sdk/Configuration.kt
+++ buildSrc/src/main/java/io/onreg/sdk/Configuration.kt
@@ -4,6 +4,6 @@ object Configuration {
     const val compileSdkVersion = 30
     const val targetSdkVersion = 30
     const val minSdkVersion = 16
-    const val versionName = "0.0.1"
+    const val versionName = "0.0.2"
     const val artifactGroup = "io.github.onreg"
 }
