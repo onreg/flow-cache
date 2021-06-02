152f8b71721761923f29734e99de33a0736e713c
Vadzim Korzun <Onreg01@gmail.com>
2021-06-02 10:25:57 +0300

    make delegates lazy

--- flow-cache/src/main/java/io/github/onreg/flowcache/FlowCache.kt
+++ flow-cache/src/main/java/io/github/onreg/flowcache/FlowCache.kt
@@ -8,36 +8,35 @@ import io.github.onreg.flowcache.cache.ParamCache
 import io.github.onreg.flowcache.cache.StatusCacheHolder
 import io.github.onreg.flowcache.model.Status
 import kotlinx.coroutines.flow.Flow
-import kotlin.properties.ReadOnlyProperty
 
 internal const val DEF_ACTION = "DEF_ACTION"
 
 fun <R : Any?> ViewModel.cache(
     start: Boolean = true,
     function: () -> Flow<R>
-): ReadOnlyProperty<ViewModel, Cache<R>> {
-    return CacheHolder(DEF_ACTION, start, viewModelScope, { function() })
+): Lazy<Cache<R>> = lazy {
+    CacheHolder(DEF_ACTION, start, viewModelScope, { function() })
 }
 
 fun <T : Any?, R : Any?> ViewModel.cache(
     initialParam: T? = null,
     start: Boolean = true,
     function: (T) -> Flow<R>
-): ReadOnlyProperty<ViewModel, ParamCache<T, R>> {
-    return CacheHolder(initialParam, start, viewModelScope, function)
+): Lazy<ParamCache<T, R>> = lazy {
+    CacheHolder(initialParam, start, viewModelScope, function)
 }
 
 fun <R : Any?> ViewModel.statusCache(
     start: Boolean = true,
     function: () -> Flow<R>
-): ReadOnlyProperty<ViewModel, Cache<Status<R>>> {
-    return StatusCacheHolder(DEF_ACTION, start, viewModelScope, { function() })
+): Lazy<Cache<Status<R>>> = lazy {
+    StatusCacheHolder(DEF_ACTION, start, viewModelScope, { function() })
 }
 
 fun <T : Any, R : Any?> ViewModel.statusCache(
     initialParam: T? = null,
     start: Boolean = true,
     function: (T) -> Flow<R>
-): ReadOnlyProperty<ViewModel, ParamCache<T, Status<R>>> {
-    return StatusCacheHolder(initialParam, start, viewModelScope, function)
+): Lazy<ParamCache<T, Status<R>>> = lazy {
+    StatusCacheHolder(initialParam, start, viewModelScope, function)
 }
